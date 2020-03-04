ff87806b89049f0c6328eaf7c542a2f4084863af
wz857 <61740097+wz857@users.noreply.github.com>
2020-03-04 13:28:55 +0800

    update

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,33 +1,7 @@
-//express_demo.js 文件
-var express = require('express');
-var app = express();
-
-const http = require("http")
-
-app.get('/', function (req, res) {
-    const url = "http://18.180.10.46:9090/"
-    var html = ""
-    http.get(url, (rs) => {
-        rs.on("data", (data) => {
-            html += data
-        })
-        rs.on("end", () => {
-            console.log(html)
-            res.send(html);
-        })
-    }).on("error", (e) => {
-        console.log(`获取数据失败: ${e.message}`)
-    })
-
-    
-})
-
-var server = app.listen(3000, function () {
-
-    var host = server.address().address
-    var port = server.address().port
-
-    console.log("应用实例，访问地址为 http://%s:%s", host, port)
-
-})
+const Koa = require('koa');
+const app = new Koa();
+app.use(async ctx => {
+  ctx.body = 'Hello docker';
+});
+app.listen(3000);
 
